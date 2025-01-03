5f87ed92b60212e425e4f49c47a95b01a8c1e9db
AICI-Global-84 <aiciglobal01@gmail.com>
2025-01-03 18:31:09 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .VideoEffect import AudioSoundWavesEffectNode
+
+NODE_CLASS_MAPPINGS = {
+    "AudioSoundWavesEffectNode": AudioSoundWavesEffectNode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AudioSoundWavesEffectNode": AudioSoundWavesEffectNode
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
